727c1eea4c8b03579ab1bd8d0957c46137a49337
Alejox102004 <116172949+Alejox102004@users.noreply.github.com>
2024-05-14 09:42:13 -0500

    cambio landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/welcome.css') }}">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
+    <title>NEXUS</title>
 </head>
 <body>
-    
+    <nav id="header">    
+        <img src="{{ asset('img/Logo.png') }}" alt="logo">
+        <ul>
+            <i class="fa-solid fa-user"></i>
+            <li class="li"> <a href="{{ route('login') }}">Iniciar Sesion</a></li>
+            <li class="li"><a href="{{ route('register') }}">Registrarse</a></li>
+        </ul>
+    </nav>
+    <div class="mensaje">
+        <h1>BIENVENIDO A NEXUS</h1>
+        <p>Desarrollamos el Software de acuerdo a tu necesidad</p>
+    </div>
 </body>
 </html>
