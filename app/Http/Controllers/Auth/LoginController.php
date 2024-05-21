8ce447e125f24c556c1bc0e53651e5d6feaefd0b
Diret03 <diegodavidrecalde@gmail.com>
2024-05-21 07:51:03 -0500

    Mensaje de login traducido

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -40,7 +40,7 @@ class LoginController extends Controller
 
     protected function redirectTo()
     {
-        session()->flash('success', 'You are logged in!');
+        session()->flash('success', 'Se ha ingresado correctamente');
         return $this->redirectTo;
     }
 }
